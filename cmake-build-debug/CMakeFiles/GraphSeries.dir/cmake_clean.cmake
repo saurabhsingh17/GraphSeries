0666b330cf3b5f2891ab50aff0fb581356aff74c
Maruti <saurabh0497@gmail.com>
2021-08-02 12:05:05 +0530

    Pushing the latest changes.

---
+++ cmake-build-debug/CMakeFiles/GraphSeries.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GraphSeries.dir/DFS/Code.cpp.obj"
+  "GraphSeries.exe"
+  "GraphSeries.exe.manifest"
+  "GraphSeries.pdb"
+  "libGraphSeries.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GraphSeries.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/GraphSeries.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for GraphSeries.
+# This may be replaced when dependencies are built.
